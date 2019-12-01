fe8b85f4b61868c2f9ce3d6a4c03eaabf7930e0c
Kabix-dk <Kabix.dk@gmail.com>
2019-12-01 17:35:17 +0100

    Add resource bundles.

--- src/main/java/pb/wi/musicweb/Main.java
+++ src/main/java/pb/wi/musicweb/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.ResourceBundle;
+
 public class Main extends Application{
 
     public static void main(String[] args) {
@@ -15,10 +17,12 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
         BorderPane borderPane = loader.load();
         Scene scene = new Scene(borderPane);
         stage.setScene(scene);
-        stage.setTitle("MusicWEB");
+        stage.setTitle(bundle.getString("title.application"));
         stage.show();
     }
 }
