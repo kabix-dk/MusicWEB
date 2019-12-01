2e9a57d5b6f4fe48059da0f57a3024677e23c83f
Kabix-dk <Kabix.dk@gmail.com>
2019-12-01 18:38:03 +0100

    Add song button and setCenter method.

--- src/main/java/pb/wi/musicweb/controllers/TopMenuButtonsController.java
+++ src/main/java/pb/wi/musicweb/controllers/TopMenuButtonsController.java
@@ -2,19 +2,34 @@ package pb.wi.musicweb.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
+    public static final String MUSIC_BASE_FXML = "/fxml/MusicBase.fxml";
+    public static final String SONG_LIST_FXML = "/fxml/SongList.fxml";
+    public static final String ADD_SONG_FXML = "/fxml/AddSong.fxml";
     private MainController mainController;
 
     @FXML
+    private ToggleGroup toggleButtons;
+
+    @FXML
     public void openBase(ActionEvent actionEvent) {
-        System.out.println("Base!\n");
+        mainController.setCenter(MUSIC_BASE_FXML);
     }
 
     @FXML
     public void openMusicList(ActionEvent actionEvent) {
-        System.out.println("Music List!\n");
+        mainController.setCenter(SONG_LIST_FXML);
+    }
+
+    @FXML
+    public void addSong(ActionEvent actionEvent) {
+        if(toggleButtons.getSelectedToggle() != null) {
+            toggleButtons.getSelectedToggle().setSelected(false);
+        }
+        mainController.setCenter(ADD_SONG_FXML);
     }
 
     public void setMainController(MainController mainController) {
